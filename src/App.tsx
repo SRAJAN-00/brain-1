650706ee305adbd09bf89b66e6d78b89b3f7fe70
srajan <you@example.com>
2025-06-04 02:18:41 -0700

    Initial commit for brain-frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { DashBoard } from "./components/DashBoard";
 import { SignIn } from "./components/SignIn";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { SignUp } from "./components/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<DashBoard />} />
